5341103c02ac503ac9d2f390a6d117f97c6bdb6c
Daniel Silva <daniel@developmentseed.org>
2015-06-12 15:16:28 +0100

    Initial implementation of scatterplot

--- app/assets/scripts/components/indicators/fairness.js
+++ app/assets/scripts/components/indicators/fairness.js
@@ -2,6 +2,41 @@
 var Reflux = require('reflux');
 var React = require('react/addons');
 var AppStore = require('../../stores/app_store');
+var ScatterplotChart = require('../shared/scatterplot_chart');
+
+
+var relationshipData = [
+  {
+    name: 'company1',
+    suppliers: 10,
+    contracts: 30,
+    amount: 25000
+  },
+  {
+    name: 'company2',
+    suppliers: 15,
+    contracts: 18,
+    amount: 40000
+  },
+  {
+    name: 'company3',
+    suppliers: 22,
+    contracts: 10,
+    amount: 60000
+  },
+  {
+    name: 'company4',
+    suppliers: 26,
+    contracts: 40,
+    amount: 66000
+  },
+  {
+    name: 'company5',
+    suppliers: 10,
+    contracts: 12,
+    amount: 80888
+  }
+];
 
 var IndFairness = module.exports = React.createClass({
   mixins: [Reflux.listenTo(AppStore, "onAppStoreData")],
@@ -49,6 +84,7 @@ var IndFairness = module.exports = React.createClass({
     }
     return (
       <div className="content">
+        <ScatterplotChart data={relationshipData} />
         <p className="desc">A level playing field is a linchpin of continuous competition for government contracts, and competition is a pre-requisite for cost-efficiency and quality.</p>
         {data}
       </div>
