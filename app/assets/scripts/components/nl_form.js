75401f3ca4fb469eeeefa7fdbbe283f5a7b88464
Daniel Silva <daniel@developmentseed.org>
2015-06-18 13:05:15 +0100

    Add animation capabilities to the nlform

--- app/assets/scripts/components/nl_form.js
+++ app/assets/scripts/components/nl_form.js
@@ -1,5 +1,6 @@
 'use strict';
 var React = require('react/addons');
+var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
 var Router = require('react-router');
 var _ = require('lodash');
 
@@ -104,7 +105,7 @@ var NlForm = module.exports = React.createClass({
    * <a href="#field1"></a>
    */
   positionFieldOptions: function() {
-    var fieldOptions = Array.prototype.slice.call(this.getDOMNode().querySelectorAll('[data-hook*="nl-position"]'));
+    var fieldOptions = Array.prototype.slice.call(document.querySelectorAll('[data-hook*="nl-position"]'));
 
     fieldOptions.forEach(function(fOpt) {
       var refLink = this.getDOMNode().querySelector('a[href="#' + fOpt.id + '"]');
@@ -192,6 +193,11 @@ var NlForm = module.exports = React.createClass({
 
       var activeOpt = _.find(fieldData.opts, {key: fieldData.active});
 
+      if (!activeOpt) {
+        console.error(new Error('NlForm -- Active option "' + fieldData.active + '" was not found for field "' + fieldName + '"'));
+        return;
+      }
+
       // Get the field index to replace it.
       var fieldIdx = sentenceParts.indexOf(fTkn);
       if (fieldIdx !== -1) {
@@ -232,7 +238,7 @@ var NlForm = module.exports = React.createClass({
 
     // Build a ul for each field.
     return fields.map(function(field) {
-      return <NlFieldOptions key={field.id} data={field} onOptionSelect={this.nlFieldOptSelect}/>
+      return <NlFieldOptions key={field.id} data={field} onOptionSelect={this.nlFieldOptSelect} />;
     }.bind(this));
   },
 
@@ -260,6 +266,7 @@ var NlForm = module.exports = React.createClass({
     return (
       <span>
         {nlSentence}
+        {/* Although rendered here the actual element is appended to the body. */}
         {nlFieldOptions}
       </span>
     );
@@ -294,14 +301,69 @@ var NlForm = module.exports = React.createClass({
 
 /**
  * React component to render the field option boxes.
+ * The element will be appended to the body and removed when no longer in use.
+ * Inspiration from http://stackoverflow.com/questions/28802179/how-to-create-a-react-modalwhich-is-append-to-body-with-transitions 
  */
 var NlFieldOptions = React.createClass({
+  element: null,
+
+  render: function() {
+    return null;
+  },
+
   onOptionClick: function(field, option, e) {
     e.preventDefault();
     this.props.onOptionSelect(field, option);
   },
 
-  render: function() {
+  componentDidMount: function() {
+    var p = document.createElement('div');
+    p.id = this.props.data.id + '-wrapper';
+    document.body.appendChild(p);
+
+    this.element = p;
+    this.componentDidUpdate();
+
+    if (document.body.className.indexOf('curtain-on') === -1) {
+      document.body.className += ' curtain-on';
+    }
+  },
+
+  componentWillUnmount: function() {
+    document.body.className = document.body.className.replace('curtain-on', '');
+
+    // When the component unmounts we have to manually remove it from the
+    // DOM since we manually moved it.
+    // By doing so we can't use ReactCSSTransitionGroup to exit the element.
+    // We can however simulate the behavior. So:
+    // 
+    // - Add the class "nl-curtain-leave".
+    // - On the next tick add "nl-curtain-leave-active"
+    // - Listen for the transition to end ensuring that we're listening to
+    // the correct event.
+    // - Remove the element when done.
+    // 
+    var elementAnim = this.element.querySelector('.nl-curtain');
+    elementAnim.className += ' nl-curtain-leave';
+    // Next tick.
+    setTimeout(function() {
+      elementAnim.className += ' nl-curtain-leave-active';
+    }, 1);
+
+    // Caveat: (this also happens with ReactCSSTransitionGroup)
+    // The "transitionend" event will fire for every transition therefore
+    // if we have two transitions, one lasting 5s and another 1s,
+    // the element will be removed as soon as the first transition finishes.
+    var transitionEnd = transitionEndEventName();
+    elementAnim.addEventListener(transitionEnd, function() {
+      if (this.element) {
+        document.body.removeChild(this.element);
+        this.element = null;
+      }
+    }.bind(this), false);
+  },
+
+  componentDidUpdate: function() {
     var field = this.props.data;
 
     var items = field.opts.map(function(opt) {
@@ -310,6 +372,38 @@ var NlFieldOptions = React.createClass({
       )
     }.bind(this));
 
-    return <ul className="nl-select" id={field.id} data-hook="nl-position stop-doc-click">{items}</ul>;
-  }
+    var data = (
+      <ReactCSSTransitionGroup component="div" transitionName="nl-curtain" transitionAppear={true}>
+        <div className="nl-curtain">
+          <ul className="nl-select" id={field.id} data-hook="nl-position stop-doc-click">{items}</ul>
+        </div>
+      </ReactCSSTransitionGroup>
+    );
+
+    React.render(data, this.element);
+  },
 });
+
+
+/**
+ * Checks what's the browser specific transitionend event to use.
+ */
+var transitionEndEventName = function() {
+  var i,
+    undefined,
+    el = document.createElement('div'),
+    transitions = {
+      'transition':'transitionend',
+      'msTransition': 'MSTransitionEnd',
+      'OTransition':'otransitionend',  // oTransitionEnd in very old Opera
+      'MozTransition':'transitionend',
+      'WebkitTransition':'webkitTransitionEnd'
+    };
+
+  for (i in transitions) {
+    if (transitions.hasOwnProperty(i) && el.style[i] !== undefined) {
+      return transitions[i];
+    }
+  }
+  throw new Error('transitionend event not supported by the browser.');
+}
