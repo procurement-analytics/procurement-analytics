743ff5a9a2d0b569b32ded46452673c01e789d7b
Daniel Silva <daniel@developmentseed.org>
2015-06-29 17:24:51 +0100

    Add comments to the popover

--- app/assets/scripts/components/charts/popover.js
+++ app/assets/scripts/components/charts/popover.js
@@ -3,18 +3,35 @@ var React = require('react/addons');
 var $ = require('jquery');
 var _ = require('lodash');
 
+/**
+ * All purpose popover.
+ * Positioned absolutely according to an anchor point.
+ *
+ * Usage:
+ *  popover.setContent(content)
+ *  popover.show(posX, posY);
+ *  popover.hide();
+ *
+ */
 function popover() {
-
   var _id = _.uniqueId('ds-popover-');
   var $popover = null;
   var _working = false;
   var _content = null;
-  var _prev_content = null;
   var _x = null;
   var _y = null;
+  // Previous values. Used to know if we need to reposition or update
+  // the popover.
+  var _prev_content = null;
   var _prev_x = null;
   var _prev_y = null;
 
+  /**
+   * Sets the popover content.
+   * 
+   * @param ReactElement
+   * Content for the popover. Can be anything supported by react. 
+   */
   this.setContent = function(ReactElement) {
     _prev_content = _content;
     _content = React.renderToStaticMarkup(
@@ -25,6 +42,19 @@ function popover() {
     return this;
   }
 
+  /**
+   * Appends the popover to #site-canvas positioning it absolutely
+   * at the specified coordinates. 
+   * If the popover already exists it will be repositioned.
+   * The anchor point for the popover is the bottom center with 8px of offset.
+   *
+   * Note: The popover needs to have content before being shown.
+   * 
+   * @param  anchorX
+   *   Where to position the popover horizontally.
+   * @param  anchorY
+   *   Where to position the popover vertically.
+   */
   this.show = function(anchorX, anchorY) {
     _prev_x = _x;
     _prev_y = _y;
@@ -92,6 +122,9 @@ function popover() {
     return this;
   }
 
+  /**
+   * Removes the popover from the DOM.
+   */
   this.hide = function() {
     $('#' + _id).remove();
     _content = null;
